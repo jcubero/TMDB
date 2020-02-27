ea314bb61b7999eb322e78dbda3fab4135489ff6
Joaquin Cubero <jcubero@intelisoft.cr>
2020-02-27 17:15:24 -0600

    Add initial Tab based project

---
+++ TmdbTest/TmdbTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TmdbTest
+//
+//  Created by Joaquin Cubero on 2/27/20.
+//  Copyright © 2020 Joaquin Cubero. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TmdbTest/TmdbTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TmdbTest
+//
+//  Created by Joaquin Cubero on 2/27/20.
+//  Copyright © 2020 Joaquin Cubero. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
